1dba53166c88a0930ae17fbec7960d09a0f95b73
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-04 19:01:25 -0400

    remove generateProfile.js and move code to index.js and and const for manager questions and empty array for team profile

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,7 @@
 const Employee = require('../lib/Employee');
+
+test('a name is passed in for the new employee', () => {
+    const name = new Name('Big Tony');
+
+    expect(name.name).toBe('Big Tony');
+});
