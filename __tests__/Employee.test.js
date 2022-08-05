ddd9047b7eeab6c780965c4be16f14669cdd62be
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-04 23:57:52 -0400

    add function for choosing team members and manager questions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,1 @@
 const Employee = require('../lib/Employee');
-
-test('a name is passed in for the new employee', () => {
-    const name = new Name('Big Tony');
-
-    expect(name.name).toBe('Big Tony');
-});
--- index.js
+++ index.js
@@ -8,17 +8,51 @@ const Manager = require('../lib/Manager');
 const Engineer = require('../lib/Engineer');
 const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
-// Array for user to add in team member information for Manager, Engineer, Employees, and Intern
-// Array for Manager input
+const generateTeam = require('../lib/generateTeam');
+const { userInfo } = require("os");
+// empty array to pass in teamProfile 
 const teamProfile = [];
 
-const manager = [
+// prompt user to choose which team member they would like to add, once user chooses that team members questions will prompt
+const chooseTeam = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'profile',
+            message: 'Choose which team member you are adding -',
+            choices: ['Manager', 'Engineer', 'Intern', 'Done']
+        }
+    ])
+    .then(teamChoice => {
+        // takes user to selected team member choices
+        switch (teamChoice.profile) {
+            // manager input array
+            case 'Manager':
+                managerInput();
+                break;
+            // engineer input array
+            case 'Engineer':
+                engineerInput();
+                break;
+            // intern input array
+            case 'Intern':
+                internInput();
+                break;
+            // will generate the user input with team members for profile through html
+            default:
+                generateProfile();
+        }
+    })
+}
+
+// Array for Manager input required fields
+const managerInput = ([
     {    
         type: 'input',
-        name: 'name',
+        name: 'managerName',
         message: 'Provide your first and last name (Required)',
-        validate: nameInput => {
-            if (nameInput) {
+        validate: managerNameInput => {
+            if (managerNameInput) {
                 return true;
             } else {
                 console.log('You need to enter a name in this required field!');
@@ -28,10 +62,10 @@ const manager = [
     },
     {
         type: 'input',
-        name: 'id',
+        name: 'managerId',
         message: 'Provide your assigned 6 digit ID number. (Required)',
-        validate: idInput => {
-            if (idInput >= 000000 && idInput <= 999999) {
+        validate: managerIdInput => {
+            if (managerIdInput >= 000000 && managerIdInput <= 999999) {
                 return true;
             } else {
                 console.log('You need to enter an ID in this required field!');
@@ -41,10 +75,10 @@ const manager = [
     },
     {
         type: 'input',
-        name: 'email',
+        name: 'managerEmail',
         message: 'Provide your assigned employee email address. (Required)',
-        validate: emailInput => {
-            if (emailInput) {
+        validate: managerEmailInput => {
+            if (managerEmailInput) {
                 return true;
             } else {
                 console.log('You need to enter an email in this required field!');
@@ -54,10 +88,10 @@ const manager = [
     },
     {
         type: 'input',
-        name: 'officeNum',
+        name: 'managerOffice',
         message: 'Provide your assigned office number. (Required)',
-        validate: officeNumInput => {
-            if (officeNumInput) {
+        validate: managerOfficeInput => {
+            if (managerOfficeInput) {
                 return true;
             } else {
                 console.log('You need to enter an office number in this required field!');
@@ -65,4 +99,9 @@ const manager = [
             }
         }
     }
-];
+])
+.then(input => {
+    const manager = new Manager(input.managerName, input.managerId, input.managerEmail, input.managerOffice);
+    teamProfile.push(manager);
+    chooseTeam();
+})
