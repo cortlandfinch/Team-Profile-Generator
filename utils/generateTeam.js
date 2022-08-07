1ba3ccee841136f890c1d4c8bae1fea66a7637af
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-07 09:03:15 -0400

    add email hrefs to employee emails

--- utils/generateTeam.js
+++ utils/generateTeam.js
@@ -2,21 +2,31 @@
 const createHTML = [];
 // function for Manager HTML input
 const createManager = manager => {
-    let managerCard = /*html*/ `
+    var managerCard = /*html*/ `
         <div class="panel active" style="background-image: url(./imgs/manager.jpeg);">
         <h3 class="title">Manager: ${manager.name}</h3>
-        <p>ID: ${manager.id}<br>Email: ${manager.email}<br> Office: ${manager.officeNumber}</p>
+        <p>ID: ${manager.id}<br>Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span><br> Office: ${manager.officeNumber}</p>
         </div>
     `;
     createHTML.push(managerCard);
 }
 
 const createEngineeer = engineer => {
-    let engineer = /*html*/ `
+    var engineer = /*html*/ `
         <div class="panel" style="background-image: url(./imgs/engineer.jpeg);">
         <h3 class="title">Engineer: ${engineer.name}</h3>
-        <p>ID: <br>Email: <br> GitHub: </p>
+        <p>ID: ${engineer.id}<br>Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span><br> GitHub: ${engineer.gitHub}</p>
         </div> 
     `;
     createHTML.push(engineerCard);
+}
+
+const createIntern = intern => {
+    var internCard = /*html*/ `
+        <div class="panel" style="background-image: url(./imgs/intern.jpeg);">
+        <h3 class="title">Intern: </h3>
+        <p>ID: ${intern.id}<br>Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span><br> School: ${intern.school}</p>
+        </div>
+    `;
+    createHTML.push(internCard)
 }
